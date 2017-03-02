364c19ac74808020a0fa65d087fe22e1bbc1fc30
nishino-naoyuki <nishino@asojuku.ac.jp>
2017-03-02 17:32:38 +0900

    3/2

--- batch/judge.sh
+++ batch/judge.sh
@@ -6,12 +6,23 @@
 ##
 #######################################
 
+FNAME=$2
+
+
 # 複雑度を判定
 cccc $1/$2 --outdir=$3/cccc
 
+if [${FNAME##*.}="java"] then
 # コンパイルを行う
 javac -d $1/classes $1/$2 2>$3/error.txt
 
 # 実行する
 cd $1/classes
 java $4 $5 $6 $7 $8 $9 > $3/result.txt
+else
+gcc $1/$2 2>$3/error.txt
+
+cd $1
+
+./a.out $5 $6 $7 $8 $9 > $3/result.txt
+fi
