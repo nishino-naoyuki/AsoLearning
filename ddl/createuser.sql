0dd7ced576d9ee1bd8cf79574911212227c5a6f7
nishino-naoyuki <nishino@asojuku.ac.jp>
2017-07-14 20:43:40 +0900

    7/14 作業文 DDL修正

--- ddl/createuser.sql
+++ ddl/createuser.sql
@@ -1,2 +1,2 @@
 CREATE USER 'aso_learning'@'localhost' IDENTIFIED BY 'aso_learning123';
-GRANT ALL PRIVILEGES ON asolearning;.* TO 'aso_learning'@'localhost';
+GRANT ALL PRIVILEGES ON asolearning.* TO 'aso_learning'@'localhost';
