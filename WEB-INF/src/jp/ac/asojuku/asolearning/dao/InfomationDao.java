ebed8bacad899051ea26b22dda43036d8c7dda01
nishino-naoyuki <nishino@asojuku.ac.jp>
2017-08-28 14:40:19 +0900

    8/28学校作業分

--- WEB-INF/src/jp/ac/asojuku/asolearning/dao/InfomationDao.java
+++ WEB-INF/src/jp/ac/asojuku/asolearning/dao/InfomationDao.java
@@ -95,9 +95,9 @@ public class InfomationDao extends Dao {
 	//7日以内に更新されたコメントを取得
 	private static final String COMMENT_INFO_RECNET =
 			"SELECT * FROM "
-			+ "(select *,datediff(CURRENT_DATE(),r.COMMENT_UPDATE_TIME) diff from RESULT_TBL r"
-			+ "LEFT JOIN TASK_TBL t ON(r.TASK_ID=t.TASK_ID)) dd "
-			+ "WHERE (dd.diff BETWEEN -7 AND 7) AND dd.USER_ID = ? ";
+			+ "RESULT_TBL r "
+			+ "LEFT JOIN TASK_TBL t ON(r.TASK_ID=t.TASK_ID) "
+			+ "WHERE datediff(CURRENT_DATE(),r.COMMENT_UPDATE_TIME) <= 7 AND r.USER_ID = ? ";
 
 
 	/**
